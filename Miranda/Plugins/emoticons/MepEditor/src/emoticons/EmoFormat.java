3d65988975c9052fd67c34c2195731e21afa0baf
Ricardo Pescuma Domenecci <ricardo@pescuma.org>
2008-04-12 16:26:21 +0000

    Fix for problems reported in the forum

--- Miranda/Plugins/emoticons/MepEditor/src/emoticons/EmoFormat.java
+++ Miranda/Plugins/emoticons/MepEditor/src/emoticons/EmoFormat.java
@@ -1,4 +1,5 @@
 package emoticons;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
--- Miranda/Plugins/emoticons/MepEditor/src/emoticons/MepEditor.java
+++ Miranda/Plugins/emoticons/MepEditor/src/emoticons/MepEditor.java
@@ -50,13 +50,19 @@ public class MepEditor
 		new EmoFormat(emoticons, protocols).load();
 		
 		Display display = new Display();
+//		DeviceData data = new DeviceData();
+//		data.tracking = true;
+//		Display display = new Display(data);
+//		Sleak sleak = new Sleak();
+//		sleak.open();
+		
 		shell = new Shell(display);
 		DirectoryDialog dialog = new DirectoryDialog(shell, SWT.NONE);
 		dialog.setText("Select folder with emoticon images");
 		String path = dialog.open();
 		if (path == null)
 			return;
-		//		String path = "C:\\Desenvolvimento\\Miranda\\bin\\Debug Unicode\\Customize\\Emoticons\\Originals";
+//		String path = "C:\\Desenvolvimento\\Miranda\\bin\\Debug Unicode\\Customize\\Emoticons\\Originals";
 		File mepPath = new File(path);
 		if (!mepPath.exists())
 			return;
@@ -64,12 +70,10 @@ public class MepEditor
 		mepFormat = new MepFormat(mepPath, emoticons, protocols);
 		mep = mepFormat.load();
 		
-		shell.setLayout(new FillLayout());
-		
 		createMainWindow(shell);
 		
 		shell.setText("Emoticon Pack Editor");
-		shell.setImage(new Image(Display.getCurrent(), "data/Defaults/smile.png"));
+		shell.setImage(Images.get("data/Defaults/smile.png"));
 		shell.setSize(500, 600);
 		shell.open();
 		
@@ -105,7 +109,7 @@ public class MepEditor
 	private static void createToolbar(Composite main)
 	{
 		Button save = new Button(main, SWT.PUSH);
-		save.setImage(new Image(Display.getCurrent(), "imgs/disk.png"));
+		save.setImage(Images.get("imgs/disk.png"));
 		save.addListener(SWT.Selection, new Listener() {
 			public void handleEvent(Event event)
 			{
@@ -119,293 +123,6 @@ public class MepEditor
 				}
 			}
 		});
-	}
-	
-	private static void createEmoticonsGroup(final Composite main, String protocol)
-	{
-		final Group group = new Group(main, SWT.NONE);
-		group.setLayoutData(new GridData(GridData.FILL_BOTH));
-		group.setText("Emoticons");
-		group.setLayout(new FillLayout());
-		
-		emoticonsScroll = new ScrolledComposite(group, SWT.V_SCROLL);
-		emoticonsComposite = new Composite(emoticonsScroll, SWT.NONE);
-		emoticonsComposite.setLayout(new GridLayout(2, false));
-		
-		createBoldLabel(emoticonsComposite, "Protocol:");
-		createProtoCombo(emoticonsComposite, protocol, main, group);
-		
-		for (final Emoticon emo : mep.emoticons)
-		{
-			EmoticonImage icon = emo.icons.get(protocol);
-			if (icon == null)
-				continue;
-			
-			createEmoticonNameLabel(emoticonsComposite, emo);
-			
-			Composite attribs = new Composite(emoticonsComposite, SWT.NONE);
-			attribs.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-			GridLayout gl = new GridLayout(2, false);
-			gl.marginWidth = 0;
-			attribs.setLayout(gl);
-			
-			createIconCombo(icon, attribs, emo, protocol);
-			createFramesHolder(emo, icon, attribs);
-		}
-		
-		emoticonsScroll.setContent(emoticonsComposite);
-		emoticonsScroll.setExpandHorizontal(true);
-		emoticonsScroll.setExpandVertical(true);
-		emoticonsScroll.addControlListener(new ControlAdapter() {
-			@Override
-			public void controlResized(ControlEvent e)
-			{
-				Rectangle r = emoticonsScroll.getClientArea();
-				emoticonsScroll.setMinSize(emoticonsComposite.computeSize(r.width, SWT.DEFAULT));
-			}
-		});
-		
-		emoticonsComposite.pack();
-		emoticonsScroll.setMinSize(emoticonsComposite.computeSize(SWT.DEFAULT, SWT.DEFAULT, false));
-	}
-	
-	private static void createEmoticonNameLabel(final Composite parent, final Emoticon emo)
-	{
-		Composite nameComposite = new Composite(parent, SWT.NONE);
-		nameComposite.setLayoutData(new GridData());
-		nameComposite.setLayout(new GridLayout(2, false));
-		
-		File defImage = new File("data/Defaults/" + emo.name + ".png");
-		if (defImage.exists())
-		{
-			try
-			{
-				Image image = new Image(Display.getCurrent(), defImage.getCanonicalPath());
-				Label label = new Label(nameComposite, SWT.NONE);
-				label.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-				label.setImage(image);
-			}
-			catch (IOException e)
-			{
-				e.printStackTrace();
-			}
-		}
-		
-		createBoldLabel(nameComposite, emo.name);
-	}
-	
-	private static void createProtoCombo(final Composite parent, String protocol, final Composite main, final Group group)
-	{
-		final Combo protoCombo = new Combo(parent, SWT.READ_ONLY);
-		protoCombo.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-		protoCombo.add("All protocols");
-		for (String p : protocols)
-			protoCombo.add(p);
-		protoCombo.select(protocol == null ? 0 : protoCombo.indexOf(protocol));
-		protoCombo.addListener(SWT.Selection, new Listener() {
-			public void handleEvent(Event e)
-			{
-				runAllDelayedListeners();
-				
-				String proto = protoCombo.getText();
-				if (!protocols.contains(proto))
-					proto = null;
-				
-				group.dispose();
-				
-				createEmoticonsGroup(main, proto);
-				
-				shell.layout();
-			}
-		});
-	}
-	
-	private static void createIconCombo(final EmoticonImage icon, Composite parent, final Emoticon emo, final String protocol)
-	{
-		Label label = new Label(parent, SWT.NONE);
-		label.setText("Icon:");
-		
-		final Combo combo = new Combo(parent, SWT.NONE);
-		combo.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-		
-		if (icon.image != null)
-			combo.setText(icon.image.path);
-		
-		combo.addListener(SWT.FocusIn, new Listener() {
-			public void handleEvent(Event event)
-			{
-				if (combo.getItemCount() > 0)
-					return;
-				
-				inGuiSetCommand = true;
-				
-				for (ImageFile file : mep.images)
-					combo.add(file.path);
-				
-				inGuiSetCommand = false;
-			}
-		});
-		
-		DelayedListener listener = new DelayedListener(new Listener() {
-			public void handleEvent(Event e)
-			{
-				icon.image = mep.createTemporaryImage(combo.getText(), protocol);
-				fillFramesHolder(emo, icon);
-			}
-		});
-		combo.addListener(SWT.Selection, listener.fast());
-		combo.addListener(SWT.Modify, listener.delayed());
-	}
-	
-	private static final Set<DelayedListener> delayedListeners = new HashSet<DelayedListener>();
-	
-	private static void runAllDelayedListeners()
-	{
-		while (delayedListeners.size() > 0)
-		{
-			delayedListeners.iterator().next().run();
-		}
-	}
-	
-	static class DelayedListener
-	{
-		private Event event;
-		private Listener listener;
-		private Color bkg;
-		private Runnable run = new Runnable() {
-			public void run()
-			{
-				DelayedListener.this.run();
-			}
-		};
-		
-		public DelayedListener(Listener listener)
-		{
-			this.listener = listener;
-		}
-		
-		private void run()
-		{
-			Display.getCurrent().timerExec(-1, run);
-			delayedListeners.remove(this);
-			((Control) event.widget).setBackground(bkg);
-			listener.handleEvent(event);
-		}
-		
-		private void start(Event e)
-		{
-			event = e;
-			
-			if (bkg == null)
-				bkg = ((Control) e.widget).getBackground();
-			((Control) e.widget).setBackground(new Color(Display.getCurrent(), 255, 255, 200));
-			
-			delayedListeners.add(this);
-			Display.getCurrent().timerExec(-1, run);
-			Display.getCurrent().timerExec(3000, run);
-		}
-		
-		public Listener delayed()
-		{
-			return new Listener() {
-				public void handleEvent(Event e)
-				{
-					if (inGuiSetCommand)
-						return;
-					
-					start(e);
-				}
-			};
-		}
-		
-		public Listener fast()
-		{
-			return new Listener() {
-				public void handleEvent(Event e)
-				{
-					if (inGuiSetCommand)
-						return;
-					
-					event = e;
-					run();
-				}
-			};
-		}
-	};
-	
-	private static void fillFramesHolder(final Emoticon emo, final EmoticonImage icon)
-	{
-		Display.getCurrent().asyncExec(new Runnable() {
-			public void run()
-			{
-				disposeAllChildren(emo.frames);
-				
-				if (icon.image == null)
-					return;
-				
-				icon.image.loadFrames();
-				if (icon.image.frames == null)
-					return;
-				
-				Listener listener = new Listener() {
-					public void handleEvent(Event e)
-					{
-						Button button = (Button) e.widget;
-						
-						Control[] children = emo.frames.getChildren();
-						for (int i = 0; i < children.length; i++)
-						{
-							Control child = children[i];
-							if (e.widget != child && child instanceof Button && (child.getStyle() & SWT.TOGGLE) != 0)
-							{
-								((Button) child).setSelection(false);
-							}
-							else if (child == button)
-							{
-								icon.image.frame = i;
-							}
-						}
-						button.setSelection(true);
-					}
-				};
-				
-				Image[] frames = icon.image.frames;
-				for (int j = 0; j < frames.length; j++)
-				{
-					Button button = new Button(emo.frames, SWT.TOGGLE);
-					button.setImage(frames[j]);
-					if (j == icon.image.frame)
-						button.setSelection(true);
-					button.addListener(SWT.Selection, listener);
-				}
-				
-				emo.frames.layout();
-				emoticonsComposite.pack();
-				Rectangle r = emoticonsScroll.getClientArea();
-				emoticonsScroll.setMinSize(emoticonsComposite.computeSize(r.width, SWT.DEFAULT));
-			}
-		});
-	}
-	
-	private static void disposeAllChildren(final Composite composite)
-	{
-		Control[] children = composite.getChildren();
-		for (int i = 0; i < children.length; i++)
-			children[i].dispose();
-	}
-	
-	private static void createFramesHolder(Emoticon emo, EmoticonImage icon, Composite parent)
-	{
-		Label label = new Label(parent, SWT.NONE);
-		label.setText("Frame:");
-		
-		emo.frames = new Composite(parent, SWT.NONE);
-		emo.frames.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-		RowLayout rowLayout = new RowLayout(SWT.HORIZONTAL);
-		rowLayout.wrap = true;
-		emo.frames.setLayout(rowLayout);
-		
-		fillFramesHolder(emo, icon);
 	}
 	
 	private static void createPackGroup(Composite main)
@@ -450,22 +167,345 @@ public class MepEditor
 		
 	}
 	
-	private static void createBoldLabel(Composite parent, String text)
+	private static void createEmoticonsGroup(final Composite main, String protocol)
+	{
+		final Group group = new Group(main, SWT.NONE);
+		group.setLayoutData(new GridData(GridData.FILL_BOTH));
+		group.setText("Emoticons");
+		group.setLayout(new FillLayout());
+		
+		emoticonsScroll = new ScrolledComposite(group, SWT.V_SCROLL);
+		emoticonsComposite = new Composite(emoticonsScroll, SWT.NONE);
+		emoticonsComposite.setLayout(new GridLayout(4, false));
+		
+		createBoldLabel(emoticonsComposite, "Protocol:", gridData(2, 1));
+		createProtoCombo(emoticonsComposite, protocol, main, group);
+		
+		for (final Emoticon emo : mep.emoticons)
+		{
+			EmoticonImage icon = emo.icons.get(protocol);
+			if (icon == null)
+				continue;
+			
+			createEmoticonNameLabel(emoticonsComposite, emo);
+			
+			createIconCombo(icon, emoticonsComposite, emo, protocol);
+			createFramesHolder(emo, icon, emoticonsComposite);
+		}
+		
+		emoticonsScroll.setContent(emoticonsComposite);
+		emoticonsScroll.setExpandHorizontal(true);
+		emoticonsScroll.setExpandVertical(true);
+		emoticonsScroll.addControlListener(new ControlAdapter() {
+			@Override
+			public void controlResized(ControlEvent e)
+			{
+				Rectangle r = emoticonsScroll.getClientArea();
+				emoticonsScroll.setMinSize(emoticonsComposite.computeSize(r.width, SWT.DEFAULT));
+			}
+		});
+		
+		emoticonsComposite.pack();
+		emoticonsScroll.setMinSize(emoticonsComposite.computeSize(SWT.DEFAULT, SWT.DEFAULT, false));
+	}
+	
+	private static void createEmoticonNameLabel(final Composite parent, final Emoticon emo)
+	{
+		File defImage = new File("data/Defaults/" + emo.name + ".png");
+		if (defImage.exists())
+		{
+			Label label = new Label(parent, SWT.NONE);
+			label.setLayoutData(gridData(1, 2));
+			label.setImage(Images.get(defImage));
+			createBoldLabel(parent, emo.name, gridData(1, 2));
+		}
+		else
+		{
+			createBoldLabel(parent, emo.name, gridData(2, 2));
+		}
+	}
+	
+	private static void createProtoCombo(final Composite parent, String protocol, final Composite main, final Group group)
+	{
+		final Combo protoCombo = new Combo(parent, SWT.READ_ONLY);
+		protoCombo.setLayoutData(gridData(GridData.FILL_HORIZONTAL, 2, 1));
+		protoCombo.add("All protocols");
+		for (String p : protocols)
+			protoCombo.add(p);
+		protoCombo.select(protocol == null ? 0 : protoCombo.indexOf(protocol));
+		protoCombo.addListener(SWT.Selection, new Listener() {
+			public void handleEvent(Event e)
+			{
+				runAllDelayedListeners();
+				
+				String proto = protoCombo.getText();
+				if (!protocols.contains(proto))
+					proto = null;
+				
+				group.dispose();
+				
+				createEmoticonsGroup(main, proto);
+				
+				shell.layout();
+			}
+		});
+	}
+	
+	private static GridData gridData(int style, int horizontalSpan, int verticalSpan)
+	{
+		GridData gd = new GridData(style);
+		gd.horizontalSpan = horizontalSpan;
+		gd.verticalSpan = verticalSpan;
+		return gd;
+	}
+	
+	private static GridData gridData(int horizontalSpan, int verticalSpan)
+	{
+		GridData gd = new GridData();
+		gd.horizontalSpan = horizontalSpan;
+		gd.verticalSpan = verticalSpan;
+		return gd;
+	}
+	
+	private static void createIconCombo(final EmoticonImage icon, Composite parent, final Emoticon emo, final String protocol)
+	{
+		Label label = new Label(parent, SWT.NONE);
+		label.setText("Icon:");
+		
+		final Combo combo = new Combo(parent, SWT.NONE);
+		combo.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		
+		if (icon.image != null)
+			combo.setText(icon.image.path);
+		
+		combo.addListener(SWT.FocusIn, new Listener() {
+			public void handleEvent(Event event)
+			{
+				if (combo.getItemCount() > 0)
+					return;
+				
+				inGuiSetCommand = true;
+				
+				for (ImageFile file : mep.images)
+					combo.add(file.path);
+				
+				inGuiSetCommand = false;
+			}
+		});
+		
+		DelayedListener listener = new DelayedListener(new Listener() {
+			public void handleEvent(Event e)
+			{
+				if (combo.getText().trim().length() <= 0)
+					icon.image = null;
+				else
+					icon.image = mep.createTemporaryImage(combo.getText(), protocol);
+				fillFramesHolder(emo, icon);
+			}
+		});
+		combo.addListener(SWT.Selection, listener.fast());
+		combo.addListener(SWT.Modify, listener.delayed());
+	}
+	
+	private static final Set<DelayedListener> delayedListeners = new HashSet<DelayedListener>();
+	
+	private static void runAllDelayedListeners()
+	{
+		while (delayedListeners.size() > 0)
+		{
+			delayedListeners.iterator().next().run();
+		}
+	}
+	
+	static class DelayedListener
+	{
+		private Event event;
+		private Listener listener;
+		private Color bkg;
+		private Runnable run = new Runnable() {
+			public void run()
+			{
+				DelayedListener.this.run();
+			}
+		};
+		private Color color;
+		
+		public DelayedListener(Listener listener)
+		{
+			this.listener = listener;
+		}
+		
+		private void run()
+		{
+			Display.getCurrent().timerExec(-1, run);
+			delayedListeners.remove(this);
+			((Control) event.widget).setBackground(bkg);
+			listener.handleEvent(event);
+			color.dispose();
+			color = null;
+		}
+		
+		private void start(Event e)
+		{
+			event = e;
+			
+			if (bkg == null)
+				bkg = ((Control) e.widget).getBackground();
+			color = new Color(Display.getCurrent(), 255, 255, 200);
+			((Control) e.widget).setBackground(color);
+			
+			delayedListeners.add(this);
+			Display.getCurrent().timerExec(-1, run);
+			Display.getCurrent().timerExec(3000, run);
+		}
+		
+		public Listener delayed()
+		{
+			return new Listener() {
+				public void handleEvent(Event e)
+				{
+					if (inGuiSetCommand)
+						return;
+					
+					start(e);
+				}
+			};
+		}
+		
+		public Listener fast()
+		{
+			return new Listener() {
+				public void handleEvent(Event e)
+				{
+					if (inGuiSetCommand)
+						return;
+					
+					event = e;
+					run();
+				}
+			};
+		}
+	};
+	
+	private static void fillFramesHolder(final Emoticon emo, final EmoticonImage icon)
+	{
+		Display.getCurrent().asyncExec(new Runnable() {
+			public void run()
+			{
+				if (emo.frames.isDisposed())
+					return;
+				
+				boolean disposed = disposeAllChildren(emo.frames);
+				
+				if (icon.image == null)
+				{
+					if (disposed)
+						layout();
+					return;
+				}
+				
+				icon.image.loadFrames();
+				if (icon.image.frames == null)
+					return;
+				
+				Listener listener = new Listener() {
+					public void handleEvent(Event e)
+					{
+						Button button = (Button) e.widget;
+						
+						Control[] children = emo.frames.getChildren();
+						for (int i = 0; i < children.length; i++)
+						{
+							Control child = children[i];
+							if (e.widget != child && child instanceof Button && (child.getStyle() & SWT.TOGGLE) != 0)
+							{
+								((Button) child).setSelection(false);
+							}
+							else if (child == button)
+							{
+								icon.image.frame = i;
+							}
+						}
+						button.setSelection(true);
+					}
+				};
+				
+				Image[] frames = icon.image.frames;
+				for (int j = 0; j < frames.length; j++)
+				{
+					Button button = new Button(emo.frames, SWT.TOGGLE);
+					button.setImage(frames[j]);
+					if (j == icon.image.frame)
+						button.setSelection(true);
+					button.addListener(SWT.Selection, listener);
+				}
+				
+				layout();
+			}
+			
+			private void layout()
+			{
+				emo.frames.layout();
+				emoticonsComposite.pack();
+				Rectangle r = emoticonsScroll.getClientArea();
+				emoticonsScroll.setMinSize(emoticonsComposite.computeSize(r.width, SWT.DEFAULT));
+			}
+		});
+	}
+	
+	private static boolean disposeAllChildren(final Composite composite)
+	{
+		Control[] children = composite.getChildren();
+		for (int i = 0; i < children.length; i++)
+			children[i].dispose();
+		return children.length > 0;
+	}
+	
+	private static void createFramesHolder(Emoticon emo, EmoticonImage icon, Composite parent)
+	{
+		Label label = new Label(parent, SWT.NONE);
+		label.setText("Frame:");
+		
+		emo.frames = new Composite(parent, SWT.NONE);
+		emo.frames.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		RowLayout rowLayout = new RowLayout(SWT.HORIZONTAL);
+		rowLayout.wrap = true;
+		emo.frames.setLayout(rowLayout);
+		
+		fillFramesHolder(emo, icon);
+	}
+	
+	private static Label createBoldLabel(Composite parent, String text)
 	{
 		Label label = new Label(parent, SWT.NONE);
 		label.setLayoutData(new GridData());
 		label.setText(text);
 		setBoldFont(label);
+		return label;
 	}
+	
+	private static Label createBoldLabel(Composite parent, String text, GridData gd)
+	{
+		Label label = new Label(parent, SWT.NONE);
+		label.setLayoutData(gd);
+		label.setText(text);
+		setBoldFont(label);
+		return label;
+	}
+	
+	private static Font boldFont;
 	
 	private static void setBoldFont(Label name)
 	{
-		Font initialFont = name.getFont();
-		FontData[] fontData = initialFont.getFontData();
-		for (int i = 0; i < fontData.length; i++)
-			fontData[i].setStyle(SWT.BOLD);
-		Font newFont = new Font(Display.getCurrent(), fontData);
-		name.setFont(newFont);
+		if (boldFont == null)
+		{
+			Font initialFont = name.getFont();
+			FontData[] fontData = initialFont.getFontData();
+			for (int i = 0; i < fontData.length; i++)
+				fontData[i].setStyle(SWT.BOLD);
+			boldFont = new Font(Display.getCurrent(), fontData);
+		}
+		name.setFont(boldFont);
 	}
 	
 }
