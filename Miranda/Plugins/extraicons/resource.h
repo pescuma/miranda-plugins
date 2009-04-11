2fa3087fda5b25edfb34492d8993fff2bec86a64
pescuma <pescuma@c086bb3d-8645-0410-b8da-73a8550f86e7>
2009-04-11 18:09:04 +0000

    extraicons: - Added visibility icons - Added handling of CLUICAPS_FLAGS2

--- Miranda/Plugins/extraicons/resource.h
+++ Miranda/Plugins/extraicons/resource.h
@@ -3,17 +3,20 @@
 // Used by resource.rc
 //
 #define IDD_OPTIONS                     119
+#define IDI_ALWAYSVIS                   120
+#define IDI_NEVERVIS                    121
+#define IDI_CHAT                        122
 #define IDC_SLOT_L                      1075
 #define IDC_SLOT                        1076
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
-//
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
 #define _APS_3D_CONTROLS                     1
-#define _APS_NEXT_RESOURCE_VALUE        120
+#define _APS_NEXT_RESOURCE_VALUE        123
 #define _APS_NEXT_COMMAND_VALUE         40005
 #define _APS_NEXT_CONTROL_VALUE         1077
 #define _APS_NEXT_SYMED_VALUE           101
