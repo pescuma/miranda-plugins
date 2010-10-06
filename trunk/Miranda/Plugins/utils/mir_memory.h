bcf6ef3be62c9778666c8f61c98fa0276e83e9a6
pescuma <pescuma@c086bb3d-8645-0410-b8da-73a8550f86e7>
2010-10-06 00:22:35 +0000

    utils: sync with berliOS

--- trunk/Miranda/Plugins/utils/mir_memory.h
+++ trunk/Miranda/Plugins/utils/mir_memory.h
@@ -1,21 +1,21 @@
 /*
- Copyright (C) 2005-2009 Ricardo Pescuma Domenecci
+Copyright (C) 2005-2009 Ricardo Pescuma Domenecci
 
- This is free software; you can redistribute it and/or
- modify it under the terms of the GNU Library General Public
- License as published by the Free Software Foundation; either
- version 2 of the License, or (at your option) any later version.
+This is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public
+License as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
 
- This is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- Library General Public License for more details.
+This is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
 
- You should have received a copy of the GNU Library General Public
- License along with this file; see the file license.txt.  If
- not, write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- Boston, MA 02111-1307, USA.
- */
+You should have received a copy of the GNU Library General Public
+License along with this file; see the file license.txt.  If
+not, write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+*/
 
 #ifndef __MIR_MEMORY_H__
 # define __MIR_MEMORY_H__
@@ -27,9 +27,14 @@
 
 static BOOL mir_is_unicode()
 {
-	char ver[1024];
-	CallService(MS_SYSTEM_GETVERSIONTEXT, (WPARAM) sizeof(ver), (LPARAM) ver);
-	return strstr(ver, "Unicode") != NULL;
+	static int is_unicode = -1;
+	if (is_unicode == -1)
+	{
+		char ver[1024];
+		CallService(MS_SYSTEM_GETVERSIONTEXT, (WPARAM) sizeof(ver), (LPARAM) ver);
+		is_unicode = (strstr(ver, "Unicode") != NULL ? 1 : 0);
+	}
+	return is_unicode;
 }
 
 
